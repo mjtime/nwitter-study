03dfb397ae62ad81bfd68727a9811dde105622ce
mjtime <min021802@gmail.com>
2026-01-08 00:06:12 +0900

    feat: 게시물 작성시간 표시

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -6,21 +6,36 @@ import { deleteDoc, doc } from "firebase/firestore";
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 3fr 1fr;
-  padding: 20px;
+  padding: 10px;
   border: 1px solid rgba(255, 255, 255, 0.5);
   border-radius: 15px;
 `;
 
-const Column = styled.div`
+const ContentColumn = styled.div`
+  padding: 10px;
   &:last-child {
     place-self: end;
   }
+`;
+
+const VisualColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  height: 100%;
+  padding: 0 10px 5px 0;
 `;
 
 const Photo = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 15px;
+`;
+
+const CreatedAt = styled.span`
+  color: rgba(255, 255, 255, 0.5);
+  font-size: 12px;
+  padding-bottom: 10px;
 `;
 
 const Username = styled.span`
@@ -40,12 +55,19 @@ const DeleteButton = styled.button`
   border: 0;
   font-size: 12px;
   padding: 5px 10px;
-  test-transform: uppercae;
+  text-transform: uppercase;
   border-radius: 5px;
   cursor: pointer;
 `;
 
-export default function Tweet({ username, image, tweet, userId, id }: ITweet) {
+export default function Tweet({
+  username,
+  image,
+  tweet,
+  userId,
+  id,
+  createdAt,
+}: ITweet) {
   const user = auth.currentUser;
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweets?");
@@ -56,16 +78,27 @@ export default function Tweet({ username, image, tweet, userId, id }: ITweet) {
     } finally {
     }
   };
+
+  const date = new Date(createdAt);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const formattedDate = `${year}.${month}.${day} ${hours}:${minutes}`;
   return (
     <Wrapper>
-      <Column>
+      <ContentColumn>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
         {user?.uid === userId ? (
           <DeleteButton onClick={onDelete}>Delete</DeleteButton>
         ) : null}
-      </Column>
-      <Column>{image ? <Photo src={image.value} /> : null}</Column>
+      </ContentColumn>
+      <VisualColumn>
+        <CreatedAt>{formattedDate}</CreatedAt>
+        {image ? <Photo src={image.value} /> : null}
+      </VisualColumn>
     </Wrapper>
   );
 }
