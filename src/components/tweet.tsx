5b627aab25a6fc43979f1add1b4ed5ba344aba00
mjtime <min021802@gmail.com>
2026-01-08 22:59:01 +0900

    feat: 게시물 수정 기능 추가
    - 수정 모드 전환 및 취소 가능
    - 입력 글자 수 표시

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import type { ITweet } from "./timeline";
 import { auth, db } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -13,9 +14,8 @@ const Wrapper = styled.div`
 
 const ContentColumn = styled.div`
   padding: 10px;
-  &:last-child {
-    place-self: end;
-  }
+  display: flex;
+  flex-direction: column;
 `;
 
 const VisualColumn = styled.div`
@@ -44,20 +44,57 @@ const Username = styled.span`
 `;
 
 const Payload = styled.p`
-  margin: 10px 0px;
+  margin: 10px 0px 20px 0px;
   font-size: 18px;
 `;
 
-const DeleteButton = styled.button`
-  background-color: tomato;
+const TextArea = styled.textarea`
+  margin: 10px 0px;
+  font-size: 18px;
+  font-family: inherit;
   color: white;
-  font-weight: 600;
-  border: 0;
+  background-color: black;
+  border: 2px solid #007fff;
+  border-radius: 10px;
+  padding: 10px;
+  width: 100%;
+  resize: none;
+
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const TextLength = styled.p<{ $isLimit: boolean }>`
+  margin-left: auto;
+  color: ${(props) => (props.$isLimit ? "#f08080" : "#ffffff80")};
   font-size: 12px;
-  padding: 5px 10px;
+`;
+
+const ActionButtons = styled.div<{ $editMode: boolean }>`
+  display: flex;
+  gap: 10px;
+  margin-top: auto;
+  padding-top: 10px;
+  color: #ffffff80;
+
+  justify-content: ${(props) =>
+    props.$editMode ? "space-between" : "flex-start"};
+`;
+const Button = styled.button<{ $btnColor?: string }>`
+  background-color: inherit;
+  color: #ffffff80;
+  border: 0;
+  font-size: 11px;
+  padding: 0px;
   text-transform: uppercase;
   border-radius: 5px;
   cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.$btnColor || "#f08080"};
+  }
 `;
 
 export default function Tweet({
@@ -69,14 +106,60 @@ export default function Tweet({
   createdAt,
 }: ITweet) {
   const user = auth.currentUser;
+  const [isLoading, setLoading] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [editedTweet, setEditedTweet] = useState(tweet);
+
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweets?");
     if (!ok || user?.uid !== userId) return;
     try {
       await deleteDoc(doc(db, "tweets", id));
     } catch (error) {
+      alert("Failed to delete the tweet. Please try again.");
     } finally {
     }
+  };
+
+  const onEdit = () => {
+    if (user?.uid !== userId) return;
+    setEditMode(true);
+  };
+
+  const onUpdate = async () => {
+    if (
+      !user ||
+      isLoading ||
+      editedTweet === "" ||
+      editedTweet.length > 180 ||
+      user.uid !== userId
+    )
+      return;
+
+    if (editedTweet === tweet) {
+      setEditMode(false);
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const tweetRef = doc(db, "tweets", id);
+
+      await updateDoc(tweetRef, {
+        tweet: editedTweet,
+        updatedAt: Date.now(),
+      });
+      setEditMode(false);
+    } catch (error) {
+      alert("Failed to update the tweet. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onCancel = () => {
+    setEditedTweet(tweet);
+    setEditMode(false);
   };
 
   const date = new Date(createdAt);
@@ -86,18 +169,50 @@ export default function Tweet({
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const formattedDate = `${year}.${month}.${day} ${hours}:${minutes}`;
+
   return (
     <Wrapper>
       <ContentColumn>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
-        {user?.uid === userId ? (
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
-        ) : null}
+        {editMode ? (
+          <>
+            <TextArea
+              value={editedTweet}
+              onChange={(e) => setEditedTweet(e.target.value)}
+              maxLength={180}
+              required
+              rows={5}
+            />
+            <TextLength $isLimit={editedTweet.length >= 180}>
+              {editedTweet.length}/180
+            </TextLength>
+          </>
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
       </ContentColumn>
       <VisualColumn>
         <CreatedAt>{formattedDate}</CreatedAt>
         {image ? <Photo src={image.value} /> : null}
+        {user?.uid === userId ? (
+          <ActionButtons $editMode={editMode}>
+            {editMode ? (
+              <>
+                <Button onClick={onCancel}>Cancel</Button>
+                <Button onClick={onUpdate} $btnColor="#73e5ebff">
+                  {isLoading ? "Loading..." : "Update"}
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button onClick={onEdit} $btnColor="#73e5ebff">
+                  Edit
+                </Button>
+                <Button onClick={onDelete}>Delete</Button>
+              </>
+            )}
+          </ActionButtons>
+        ) : null}
       </VisualColumn>
     </Wrapper>
   );
