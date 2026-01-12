6d11886f63e3449d601073104d970124f12148be
mjtime <min021802@gmail.com>
2026-01-13 00:17:31 +0900

    feat: 프로필 사진 설정 기능 구현
    - Base64 형식을 사용하여 이미지를 Firestore에 저장
    - useEffect를 활용한 프로필 이미지 자동 불러오기 로직 추가
    - 300KB 용량 제한 처리
    - lecture: 5.0 User Avatar

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,123 @@
+import styled from "styled-components";
+import { auth, db } from "../firebase";
+import { useEffect, useState } from "react";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+const Name = styled.span`
+  font-size: 22px;
+`;
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState<string | null>(null);
+  const MAX_FILE_SIZE = 300 * 1024;
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      if (!user) return;
+
+      try {
+        const docRef = doc(db, "avatars", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setAvatar(docSnap.data().avatar);
+        }
+      } catch (e) {
+        alert("Failed to loading the avatar. Please try again.");
+      }
+    };
+
+    fetchAvatar();
+  }, [user]);
+
+  const onAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!user || !files || files.length !== 1) return;
+    const file = files[0];
+
+    if (file.size > MAX_FILE_SIZE) {
+      alert("300KB 이하의 이미지만 업로드 가능합니다.");
+      e.target.value = "";
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = async () => {
+      const base64Data = reader.result as string;
+
+      try {
+        const userDocRef = doc(db, "avatars", user.uid);
+
+        await setDoc(
+          userDocRef,
+          {
+            avatar: base64Data,
+          },
+          { merge: true }
+        );
+
+        setAvatar(base64Data);
+      } catch (error) {
+        console.log(error);
+        alert("Failed to update the avatar. Please try again.");
+      }
+    };
+
+    reader.readAsDataURL(file);
+  };
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {Boolean(avatar) ? (
+          <AvatarImg src={avatar ?? ""} />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ?? "Anonymous"}</Name>
+    </Wrapper>
+  );
 }
