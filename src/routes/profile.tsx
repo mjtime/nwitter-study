c12430dc13441b68a8ab94a626e038f20e6e525c
mjtime <min021802@gmail.com>
2026-01-17 23:25:30 +0900

    feat: 닉네임 변경 기능 구현
    - 닉네임 수정 취소(onCancel) 및 수정 시 입력 길이(10자) 제한
    - 닉네임 변경 시 작성한 모든 트윗의 작성자명 일괄 업데이트 적용

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -11,8 +11,10 @@ import {
   query,
   setDoc,
   where,
+  writeBatch,
 } from "firebase/firestore";
 import Tweet from "../components/tweet";
+import { updateProfile } from "firebase/auth";
 
 const Wrapper = styled.div`
   display: flex;
@@ -40,9 +42,66 @@ const AvatarImg = styled.img`
 const AvatarInput = styled.input`
   display: none;
 `;
+const NameWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const NameContainer = styled.div<{ $editMode: boolean }>`
+  display: flex;
+  align-items: center;
+  position: relative;
+  border-bottom: ${(props) => (props.$editMode ? "1px solid #1d9bf0" : "none")};
+`;
 const Name = styled.span`
   font-size: 22px;
 `;
+
+const BaseButton = styled.div`
+  position: absolute;
+  cursor: pointer;
+  width: 18px;
+  display: flex;
+  align-items: center;
+  svg {
+    fill: white;
+  }
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const Button = styled(BaseButton)`
+  left: 100%;
+  margin-left: 5px;
+`;
+
+const CancelButton = styled(BaseButton)`
+  right: 100%;
+  margin-right: 5px;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  font-size: 22px;
+  text-align: center;
+  background: transparent;
+  color: white;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const TextLength = styled.span`
+  font-size: 12px;
+  color: #ffffff80;
+`;
+
 const Tweets = styled.div`
   display: flex;
   width: 100%;
@@ -62,7 +121,10 @@ export interface ITweet {
 
 export default function Profile() {
   const user = auth.currentUser;
+  const [isLoading, setIsLoading] = useState(false);
   const [avatar, setAvatar] = useState<string | null>(null);
+  const [editMode, setEditMode] = useState(false);
+  const [newName, setNewName] = useState(user?.displayName ?? "");
   const MAX_FILE_SIZE = 300 * 1024;
   const [tweets, setTweets] = useState<ITweet[]>([]);
   useEffect(() => {
@@ -148,6 +210,53 @@ export default function Profile() {
 
     reader.readAsDataURL(file);
   };
+
+  const onEdit = () => {
+    setEditMode(true);
+  };
+
+  const onCancel = () => {
+    setNewName(user?.displayName ?? "");
+    setEditMode(false);
+  };
+
+  const onUpdate = async () => {
+    if (!user || newName === "" || isLoading) return;
+    if (user.displayName === newName) {
+      setEditMode(false);
+      return;
+    }
+    try {
+      setIsLoading(true);
+      await updateProfile(user, { displayName: newName });
+
+      const tweetsRef = collection(db, "tweets");
+      const q = query(tweetsRef, where("userId", "==", user.uid));
+      const snapshot = await getDocs(q);
+
+      const batch = writeBatch(db);
+      snapshot.docs.forEach((doc) => {
+        batch.update(doc.ref, { username: newName });
+      });
+
+      await batch.commit();
+
+      setTweets((prev) =>
+        prev.map((tweet) => {
+          if (tweet.userId === user.uid) {
+            return { ...tweet, username: newName };
+          }
+          return tweet;
+        })
+      );
+
+      setEditMode(false);
+    } catch (e) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
@@ -174,7 +283,72 @@ export default function Profile() {
         type="file"
         accept="image/*"
       />
-      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <NameWrapper>
+        <NameContainer $editMode={editMode}>
+          {editMode ? (
+            <>
+              <CancelButton onClick={onCancel}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </CancelButton>
+              <Input
+                onChange={(e) => setNewName(e.target.value)}
+                value={newName}
+                type="text"
+                maxLength={10}
+                required
+              />
+              <TextLength>{newName.length}</TextLength>
+              <Button onClick={onUpdate}>
+                {isLoading ? (
+                  "Loading..."
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+              </Button>
+            </>
+          ) : (
+            <>
+              <Name>{user?.displayName ?? "Anonymous"}</Name>
+              <Button onClick={onEdit}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Button>
+            </>
+          )}
+        </NameContainer>
+      </NameWrapper>
       <Tweets>
         {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
